23a270317bfe06ccd4810fb0bfa9eb07eb35927f
Chad Crum <chaddcrum@gmail.com>
2025-11-23 05:22:24 -0500

    Slightly reduce mobile web font sizes for transactions
    - Reduced main transaction text (payee and amount) from 18px to 17px
    - Reduced category text from 15px to 14px
    - Reduced date headings from 16px to 15px
    - Reduced notes and running balance from 15px to 14px

--- packages/desktop-client/src/components/mobile/transactions/TransactionListItem.tsx
+++ packages/desktop-client/src/components/mobile/transactions/TransactionListItem.tsx
@@ -58,7 +58,7 @@ const getTextStyle = ({
   isPreview: boolean;
 }): CSSProperties => ({
   ...styles.text,
-  fontSize: 18,
+  fontSize: 17,
   ...(isPreview
     ? {
         fontStyle: 'italic',
@@ -261,7 +261,7 @@ export function TransactionListItem({
                     )}
                     <TextOneLine
                       style={{
-                        fontSize: 15,
+                        fontSize: 14,
                         marginTop: 1,
                         fontWeight: '400',
                         color: prettyCategory
@@ -281,7 +281,7 @@ export function TransactionListItem({
                 {notes && (
                   <TextOneLine
                     style={{
-                      fontSize: 15,
+                      fontSize: 14,
                       marginTop: 4,
                       fontWeight: '400',
                       color: theme.tableText,
@@ -307,7 +307,7 @@ export function TransactionListItem({
                 {showRunningBalance && runningBalance !== undefined && (
                   <Text
                     style={{
-                      fontSize: 15,
+                      fontSize: 14,
                       fontWeight: '400',
                       ...makeBalanceAmountStyle(runningBalance),
                     }}
