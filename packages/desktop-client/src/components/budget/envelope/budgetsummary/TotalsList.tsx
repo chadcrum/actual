02ceec0afa69deec1f5b328a73d88568c4d0affe
Chad Crum <chaddcrum@gmail.com>
2025-12-12 20:44:59 -0500

    feat: add test data header to budget modal

--- packages/desktop-client/src/components/budget/envelope/budgetsummary/TotalsList.tsx
+++ packages/desktop-client/src/components/budget/envelope/budgetsummary/TotalsList.tsx
@@ -4,6 +4,7 @@ import { Trans } from 'react-i18next';
 import { AlignedText } from '@actual-app/components/aligned-text';
 import { Block } from '@actual-app/components/block';
 import { styles } from '@actual-app/components/styles';
+import { theme } from '@actual-app/components/theme';
 import { Tooltip } from '@actual-app/components/tooltip';
 import { View } from '@actual-app/components/view';
 
@@ -22,119 +23,135 @@ export function TotalsList({ prevMonthName, style }: TotalsListProps) {
   return (
     <View
       style={{
-        flexDirection: 'row',
-        lineHeight: 1.5,
-        justifyContent: 'center',
         ...styles.smallText,
         ...style,
+        flexDirection: 'column',
       }}
     >
       <View
         style={{
-          textAlign: 'right',
-          marginRight: 10,
-          minWidth: 50,
+          borderBottom: '1px solid ' + theme.tableBorder,
+          marginBottom: 5,
+          alignItems: 'center',
+          paddingBottom: 2,
         }}
       >
-        <Tooltip
-          style={{ ...styles.tooltip, lineHeight: 1.5, padding: '6px 10px' }}
-          content={
-            <>
-              <AlignedText
-                left="Income:"
-                right={
-                  <EnvelopeCellValue
-                    binding={envelopeBudget.totalIncome}
-                    type="financial"
-                  />
-                }
-              />
-              <AlignedText
-                left="From Last Month:"
-                right={
-                  <EnvelopeCellValue
-                    binding={envelopeBudget.fromLastMonth}
-                    type="financial"
-                  />
-                }
-              />
-            </>
-          }
-          placement="bottom end"
+        <Block>test</Block>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          lineHeight: 1.5,
+          justifyContent: 'center',
+        }}
+      >
+        <View
+          style={{
+            textAlign: 'right',
+            marginRight: 10,
+            minWidth: 50,
+          }}
         >
+          <Tooltip
+            style={{ ...styles.tooltip, lineHeight: 1.5, padding: '6px 10px' }}
+            content={
+              <>
+                <AlignedText
+                  left="Income:"
+                  right={
+                    <EnvelopeCellValue
+                      binding={envelopeBudget.totalIncome}
+                      type="financial"
+                    />
+                  }
+                />
+                <AlignedText
+                  left="From Last Month:"
+                  right={
+                    <EnvelopeCellValue
+                      binding={envelopeBudget.fromLastMonth}
+                      type="financial"
+                    />
+                  }
+                />
+              </>
+            }
+            placement="bottom end"
+          >
+            <EnvelopeCellValue
+              binding={envelopeBudget.incomeAvailable}
+              type="financial"
+            >
+              {props => <CellValueText {...props} style={{ fontWeight: 600 }} />}
+            </EnvelopeCellValue>
+          </Tooltip>
+
           <EnvelopeCellValue
-            binding={envelopeBudget.incomeAvailable}
+            binding={envelopeBudget.lastMonthOverspent}
             type="financial"
           >
-            {props => <CellValueText {...props} style={{ fontWeight: 600 }} />}
+            {props => (
+              <CellValueText
+                {...props}
+                style={{ fontWeight: 600 }}
+                formatter={(value, type) => {
+                  const v = format(value, type);
+                  return value > 0 ? '+' + v : value === 0 ? '-' + v : v;
+                }}
+              />
+            )}
           </EnvelopeCellValue>
-        </Tooltip>
 
-        <EnvelopeCellValue
-          binding={envelopeBudget.lastMonthOverspent}
-          type="financial"
-        >
-          {props => (
-            <CellValueText
-              {...props}
-              style={{ fontWeight: 600 }}
-              formatter={(value, type) => {
-                const v = format(value, type);
-                return value > 0 ? '+' + v : value === 0 ? '-' + v : v;
-              }}
-            />
-          )}
-        </EnvelopeCellValue>
+          <EnvelopeCellValue
+            binding={envelopeBudget.totalBudgeted}
+            type="financial"
+          >
+            {props => (
+              <CellValueText
+                {...props}
+                style={{ fontWeight: 600 }}
+                formatter={(value, type) => {
+                  const v = format(value, type);
+                  return value > 0 ? '+' + v : value === 0 ? '-' + v : v;
+                }}
+              />
+            )}
+          </EnvelopeCellValue>
 
-        <EnvelopeCellValue
-          binding={envelopeBudget.totalBudgeted}
-          type="financial"
-        >
-          {props => (
-            <CellValueText
-              {...props}
-              style={{ fontWeight: 600 }}
-              formatter={(value, type) => {
-                const v = format(value, type);
-                return value > 0 ? '+' + v : value === 0 ? '-' + v : v;
-              }}
-            />
-          )}
-        </EnvelopeCellValue>
+          <EnvelopeCellValue
+            binding={envelopeBudget.forNextMonth}
+            type="financial"
+          >
+            {props => (
+              <CellValueText
+                {...props}
+                style={{ fontWeight: 600 }}
+                formatter={(value, type) => {
+                  const v = format(Math.abs(value), type);
+                  return value >= 0 ? '-' + v : '+' + v;
+                }}
+              />
+            )}
+          </EnvelopeCellValue>
+        </View>
 
-        <EnvelopeCellValue
-          binding={envelopeBudget.forNextMonth}
-          type="financial"
-        >
-          {props => (
-            <CellValueText
-              {...props}
-              style={{ fontWeight: 600 }}
-              formatter={(value, type) => {
-                const v = format(Math.abs(value), type);
-                return value >= 0 ? '-' + v : '+' + v;
-              }}
-            />
-          )}
-        </EnvelopeCellValue>
-      </View>
+        <View>
+          <Block>
+            <Trans>Available funds</Trans>
+          </Block>
 
-      <View>
-        <Block>
-          <Trans>Available funds</Trans>
-        </Block>
+          <Block>
+            <Trans>Overspent in {{ prevMonthName }}</Trans>
+          </Block>
 
-        <Block>
-          <Trans>Overspent in {{ prevMonthName }}</Trans>
-        </Block>
+          <Block>
+            <Trans>Budgeted</Trans>
+          </Block>
 
-        <Block>
-          <Trans>Budgeted</Trans>
-        </Block>
-
-        <Block>
-          <Trans>For next month</Trans>
-        </Block>
+          <Block>
+            <Trans>For next month</Trans>
+          </Block>
+        </View>
       </View>
     </View>
   );
