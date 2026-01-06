8ad14a7c681e1a17f60a66f2fc8d03a371a6660b
Chad Crum <chaddcrum@gmail.com>
2026-01-06 13:19:52 -0500

    feat: add Planning link to mobile navigation

--- packages/desktop-client/src/components/FinancesApp.tsx
+++ packages/desktop-client/src/components/FinancesApp.tsx
@@ -395,6 +395,7 @@ export function FinancesApp() {
               <Route path="/rules" element={<MobileNavTabs />} />
               <Route path="/payees" element={<MobileNavTabs />} />
               <Route path="/schedules" element={<MobileNavTabs />} />
+              {planningEnabled && <Route path="/planning" element={<MobileNavTabs />} />}
               <Route path="*" element={null} />
             </Routes>
           </ScrollProvider>
