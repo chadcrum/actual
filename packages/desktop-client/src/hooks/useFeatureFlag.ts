d929b87449ff5aa7735d0d0564536c30f8283679
Chad Crum <chaddcrum@gmail.com>
2026-01-06 13:17:03 -0500

    feat: register enablePlanningPage flag with default false

--- packages/desktop-client/src/hooks/useFeatureFlag.ts
+++ packages/desktop-client/src/hooks/useFeatureFlag.ts
@@ -14,6 +14,7 @@ const DEFAULT_FEATURE_FLAG_STATE: Record<FeatureFlag, boolean> = {
   enableOverviewPage: false,
   plugins: false,
   mobileParity: false,
+  enablePlanningPage: false,
 };
 
 export function useFeatureFlag(name: FeatureFlag): boolean {
